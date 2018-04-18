c43a35c6956957df963c42ed05f022c888d48115
Pamela Chong <pjchong91@gmail.com>
2018-04-18 11:47:21 -0600

    Cleaned up project files.

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+console.log("Bebness is bestness");
+
+function subAlert() {
+  alert("Thanks for subscribing!!");
+}
+
+
+// $('.main-carousel').flickity({
+//     // options
+//     cellAlign: 'left',
+//     contain: true
+//   });
+
+//   console.log
+
+
+//   let cartitems = 0
+//   for each time additem is pressed
+//     cartitems = cartitems+1
+
+//     console.log(cartitems)
+
+
+
+//     var count = 0;
+// for (var i = 1; i < 100; i++) {
+//   count = i;
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//     }
+//   else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     } else {
+//       console.log("Current count: " + count);
+//     }
+    
+        
+      
+    
+// }
