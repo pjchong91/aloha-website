db6465ab88fb55c8946731c44cc44c56232ab07b
Pamela Chong <pjchong91@gmail.com>
2018-04-19 23:05:50 -0600

    Transfer to desktop end of Apr 19.

--- js/scripts.js
+++ js/scripts.js
@@ -16,52 +16,35 @@ $(document).ready(function(){
   
   console.log("Bebness is bestness");
 
-  //This section checks email for presence of @ and . 
-  var x = 
+
+ // Subscribe email
+
+$("#subscribe").on('click', 'button', function(){
+    var sinput = 
     document.getElementsByClassName("emailInput")[0].value;
-  var y = 
-    x.length;
-
-  $(".emailInput").css("color","blue");
-
-
-  $('input').on('keyup', function(){ //Limitation: cannot disable button once enabled (ie. through deletion)
-    var sinput = this.value;
 
     var revSinput = sinput.split("").reverse().join("");
 
     var ratIndex = revSinput.indexOf('@')
 
     var rdotIndex = revSinput.indexOf('.');
+
   
     console.log(sinput, revSinput);
     console.log(rdotIndex, ratIndex);
+   
+// This checks for presence of @ and a '.' after @.  Would reccomend using regex to improve project.
+   if (ratIndex>1 && rdotIndex>1 && rdotIndex<ratIndex) {
+        alert("Thanks for subscribing!  Expect updates from us shortly to your email: " + (sinput));
+   } else {
+        alert("Please submit a valid email.")
+};
 
-    
-  });
 
-  $("#subscribe").on("submit"), function(event) {
-    event.preventDefault();
-  }
+});
 
-  // $("#subscribe").on("submit", function(event) {
-  //   event.preventDefault();
-  //   if (ratIndex>1 && rdotIndex>1 && rdotIndex<ratIndex) {
-  //     alert("Thanks for subscribing!  Expect updates from us shortly to your email: " + (sinput));
-  //   } else {
-  //     alert("Please submit a valid email.");
-  //   }
-  // });
-  
 
-  // $("#subscribe").on('submit', (function(){
-  //   event.preventDefault();
 
-  //   if (ratIndex>1 && rdotIndex>1 && rdotIndex<ratIndex){
-  //   alert("Thanks for subscribing!  Expect updates from us shortly to your email: " + (sinput));
-  // } else {
-  //   alert("Please submit a valid email.");
-  // }));
   
 
 // JS for item count in the cart - responds to "Add to Cart" button
@@ -75,11 +58,5 @@ $(".cartAdd").on("click", function(){
   document.getElementsByClassName("cartCount")[0].innerHTML = c;
 });
 
-  
-  // $('.main-carousel').flickity({
-  //     // options
-  //     cellAlign: 'left',
-  //     contain: true
-  //   });
-
+ 
 });  //end of document.ready()
